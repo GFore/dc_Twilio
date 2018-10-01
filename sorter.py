337e24e9b0d5e659e6afce15f52e2482fb97c89e
Greg Foreman <gfore@mindspring.com>
2018-09-30 23:35:58 -0400

    adds sorter.py

---
+++ sorter.py
@@ -0,0 +1,33 @@
+# Program that gets list of unsorted numbers, a sort method, 
+# and a sort order (ascending or descending) from user and
+# returns the list sorted as requested
+
+
+def bubblesort(nlist, descending):
+
+    if descending:
+        # nlist.sort(reverse=True)
+        end_pos = len(nlist)-1
+
+        while end_pos > 1:
+            for i in range(end_pos):
+                if nlist[i] < nlist[i+1]:  # then swap them
+                    nlist[i], nlist[i+1] = nlist[i+1], nlist[i]
+                print(num_list, end_pos)
+            end_pos -= 1
+    else:
+        end_pos = len(nlist)-1
+
+        while end_pos > 1:
+            for i in range(end_pos):
+                if nlist[i] > nlist[i+1]:  # then swap them
+                    nlist[i], nlist[i+1] = nlist[i+1], nlist[i]
+                print(num_list, end_pos)
+            end_pos -= 1
+    
+
+# num_list = [1, 222, 1000, 10, 0, 9, 7, 8, 1, 3, 6, 5, 2, 4]
+num_list = [1, 222, 1000, 10, 0]
+print(num_list)
+bubblesort(num_list, True)
+print(num_list)
