700215250bd250f83ee2c431853edbb293f62979
Greg Foreman <gfore@mindspring.com>
2018-10-02 13:28:22 -0400

    combined send program into receive program

--- receive_sms.py
+++ receive_sms.py
@@ -1,13 +1,14 @@
 import os
+from twilio.rest import Client
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
-#from sorter import bubblesort
+
 def bubblesort(nlist, descending):
 
     if descending:
         end_pos = len(nlist)-1
 
-        while end_pos > 1:
+        while end_pos > 0:
             for i in range(end_pos):
                 if nlist[i] < nlist[i+1]:  # then swap them
                     nlist[i], nlist[i+1] = nlist[i+1], nlist[i]
@@ -16,7 +17,7 @@ def bubblesort(nlist, descending):
     else:
         end_pos = len(nlist)-1
 
-        while end_pos > 1:
+        while end_pos > 0:
             for i in range(end_pos):
                 if nlist[i] > nlist[i+1]:  # then swap them
                     nlist[i], nlist[i+1] = nlist[i+1], nlist[i]
@@ -24,6 +25,18 @@ def bubblesort(nlist, descending):
                 
             end_pos -= 1
 
+# Twilio account info are cell numbers are stored in env variables accessed via os.environ.get()
+
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ.get('TWILIO_AUTH_TKN')
+client = Client(account_sid, auth_token)
+
+msg = client.messages.create(
+    to=os.environ.get('CELL_PH_NUM'),
+    # to='+15551234567',            USE FOR DEMO WITH DIFFERENT CELL NUM, MUST CONFIRM IT IN TWILIO FIRST
+    from_=os.environ.get('TWILIO_PH_NUM'),
+    body='Respond with your sort method (A=Bubble, B=Merge, C=Selection) followed by a space, sort order (A=Ascending D=Descending) followed by a space, and then your space-separated, unsorted list of integers. I will return the list sorted.'
+)
 
 app = Flask(__name__)
 
