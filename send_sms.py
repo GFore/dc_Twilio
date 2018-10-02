9c53df6258cb044ab1a2511d56d544900706e0b9
Greg Foreman <gfore@mindspring.com>
2018-10-02 11:05:56 -0400

    sets Twilio acct details to local env variables

--- send_sms.py
+++ send_sms.py
@@ -1,14 +1,17 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.environ.get('TWILIO_ACCT_SID')
+# Twilio account info are cell numbers are stored in env variables accessed via os.environ.get()
+
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 auth_token = os.environ.get('TWILIO_AUTH_TKN')
 client = Client(account_sid, auth_token)
 
 msg = client.messages.create(
     to=os.environ.get('CELL_PH_NUM'),
+    # to='+15551234567',            USE FOR DEMO WITH DIFFERENT CELL NUM, MUST CONFIRM IT IN TWILIO FIRST
     from_=os.environ.get('TWILIO_PH_NUM'),
-    body='Please respond with your sort method (A=Bubble, B=Merge, C=Selection) followed by a space and then your space-separated, unsorted list of integers to be sorted.'
+    body='Respond with your sort method (A=Bubble, B=Merge, C=Selection) followed by a space, sort order (A=Ascending D=Descending) followed by a space, and then your space-separated, unsorted list of integers. I will return the list sorted.'
 )
 
 print(msg.body)
